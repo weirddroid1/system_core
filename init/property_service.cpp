2111b2281991edf4f96cd7fea1a26555d9cd1fc9
Abhay Singh Gill <abhaygill017@gmail.com>
2025-10-24 22:00:20 +0530

    init: property_service: Hide adbd
    Some apps look for it to check if dev options are enabled.
    
    Change-Id: I29ed775a5d2a14e663331a99c1b79ce998d55ada
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -400,7 +400,8 @@ static std::optional<uint32_t> PropertySet(const std::string& name, const std::s
         prop_info* pi = (prop_info*)__system_property_find(name.c_str());
         if (pi != nullptr) {
             // ro.* properties are actually "write-once", unless the system decides to
-            if (StartsWith(name, "ro.") && !weaken_prop_override_security) {
+            if ((StartsWith(name, "ro.") || name == "init.svc.adbd")
+                    && !weaken_prop_override_security) {
                 *error = "Read-only property was already set";
                 return {PROP_ERROR_READ_ONLY_PROPERTY};
             }
@@ -1150,6 +1151,8 @@ static void property_initialize_ro_vendor_api_level() {
 
 static void SetIntegrityProps() {
     bool is_debuggable = android::base::GetBoolProperty("ro.debuggable", false);
+
+    InitPropertySet("init.svc.adbd", "stopped");
 
     if (is_debuggable)
             return;
