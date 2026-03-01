6eff4156c6139e8d4cdb8932390f279fa7b8d24b
mosimchah <mosimchah@gmail.com>
2026-03-01 02:03:21 -0500

    core: AICP-ify
    Change-Id: I1367d35919f5e2c6aec4fd12ea661a9bd189abd9

--- debuggerd/libdebuggerd/tombstone_proto_to_text.cpp
+++ debuggerd/libdebuggerd/tombstone_proto_to_text.cpp
@@ -700,7 +700,7 @@ static void print_guest_thread(CallbackType callback, SymbolizeCallbackType symb
 bool tombstone_proto_to_text(const Tombstone& tombstone, CallbackType callback,
                              SymbolizeCallbackType symbolize, bool display_sp) {
   CBL("*** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***");
-  CBL("LineageOS Version: '%s'", tombstone.lineage_version().c_str());
+  CBL("AICP Version: '%s'", tombstone.lineage_version().c_str());
   CBL("Build fingerprint: '%s'", tombstone.build_fingerprint().c_str());
   if (!tombstone.kernel_release().empty()) {
     CBL("Kernel Release: '%s'", tombstone.kernel_release().c_str());
